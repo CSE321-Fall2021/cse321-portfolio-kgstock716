1b44a74f91605853f6ab76a4aaa33889a237acb1
Kat Stock <60624370+kgstock716@users.noreply.github.com>
2021-09-30 13:05:59 -0400

    Add files via upload

---
+++ Project 1/CSE321_project1_kgstock_template.cpp
@@ -0,0 +1,25 @@
+/** 
+  * Author: Katherine Stock (kgstock)
+  * 
+  * File Purpose: 
+  * Modules: 
+  * Assignment:
+  *
+  * Inputs:  
+  * Outputs:
+  * 
+  * Constraints:
+  * References: 
+  * 
+  */ 
+
+#include "mbed.h"
+
+// // main() runs in its own thread in the OS
+int main()
+{
+    while (true) {
+
+    }
+}
+
